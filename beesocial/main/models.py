d2b8b679948d6b5fa681de4273c05686194f049b
Boraniki <57863390+Boraniki@users.noreply.github.com>
2023-01-13 01:20:16 +0300

    'changes'

--- beesocial/main/models.py
+++ beesocial/main/models.py
@@ -2,20 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Intern(models.Model):
+class Courses(models.Model):
     crn = models.IntegerField(max_length=8)
-    course_code = models.IntegerField(max_length=8)
+    course_code = models.CharField(max_length=100)
     course_title = models.CharField(max_length=100)
-    teaching_method = models.CharField(max_length=100)
-    instructor = models.CharField(max_length=100)
-    building =  models.CharField(max_length=100)
+    teaching_method = models.CharField(max_length=100, default=None, blank=True, null=True)
+    instructor = models.CharField(max_length=100, default=None, blank=True, null=True)
+    building =  models.CharField(max_length=100, default=None, blank=True, null=True)
     day = models.CharField(max_length=100)
     start_time =  models.CharField(max_length=100)
     end_time =  models.CharField(max_length=100)
-    room = models.CharField(max_length=100)
-    capacity = models.IntegerField(max_length=8)
-    enrolled = models.IntegerField(max_length=8)
-    major_restriction = models.CharField(max_length=100)
-    prerequisites = models.CharField(max_length=100)
-    class_rest = models.CharField(max_length=100)
-    
+    room = models.CharField(max_length=100, default=None, blank=True, null=True)
+    capacity = models.IntegerField(max_length=8, default=None, blank=True, null=True)
+    enrolled = models.IntegerField(max_length=8, default=None, blank=True, null=True)
+    major_restriction = models.CharField(max_length=100, default=None, blank=True, null=True)
+    prerequisites = models.CharField(max_length=100, default=None, blank=True, null=True)
+    class_rest = models.CharField(max_length=100, default=None, blank=True, null=True)
--- beesocial/main/views.py
+++ beesocial/main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/index.html')
+    data = Courses.objects.all()
+    form = CoursesForm()
+    # form = CoursesForm()
+    # if request.method == 'POST':
+    #     form = CoursesForm(request.POST)
+    #     data = Courses.objects.filter(tenant=request.POST['tenant'])
+    # context = {'data':data, 'form':form}
+    return render(request, 'main/index.html', {'form':form,'data':data})
