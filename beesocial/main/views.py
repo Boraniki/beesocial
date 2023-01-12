b6b63619412146d8359ca8cf8171858150e5b0fc
Boraniki <57863390+Boraniki@users.noreply.github.com>
2023-01-13 02:14:39 +0300

    'changes'

--- beesocial/main/views.py
+++ beesocial/main/views.py
@@ -4,11 +4,14 @@ from .forms import *
 
 # Create your views here.
 def home(request):
-    data = Courses.objects.all()
     form = CoursesForm()
-    # form = CoursesForm()
-    # if request.method == 'POST':
-    #     form = CoursesForm(request.POST)
-    #     data = Courses.objects.filter(tenant=request.POST['tenant'])
-    # context = {'data':data, 'form':form}
-    return render(request, 'main/index.html', {'form':form,'data':data})
+    if request.method == 'POST':
+        form = CoursesForm(request.POST)
+        data = Courses.objects.filter(crn=request.POST['crn'],
+                                      course_code=request.POST['course_code'],
+                                      course_title=request.POST['course_title'])
+        context = {'data':data, 'form':form}
+        return render(request, 'main/index.html', {'form':form,'data':data[:10]})
+
+    context = {'form':form}
+    return render(request, 'main/index.html', {'form':form})
