4bfe632f7498e2ff7b8d9dc1850439e320c37245
eminozata <77062312+eminozata@users.noreply.github.com>
2023-01-13 02:13:15 +0300

    day seperation

--- beesocial/main/views.py
+++ beesocial/main/views.py
@@ -4,11 +4,14 @@ from .forms import *
 
 # Create your views here.
 def home(request):
-    data = Courses.objects.all()
+    crns = [21927, 21924, 22074,21929,21884]
+    data = Courses.objects.filter(crn__in=crns)
+    print(type(data))
+
     form = CoursesForm()
     # form = CoursesForm()
     # if request.method == 'POST':
     #     form = CoursesForm(request.POST)
     #     data = Courses.objects.filter(tenant=request.POST['tenant'])
     # context = {'data':data, 'form':form}
-    return render(request, 'main/index.html', {'form':form,'data':data})
+    return render(request, 'main/index.html', {'form':form,'data':data[:10]})
